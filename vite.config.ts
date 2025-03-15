b42c4980f7e91e79b2e1c563f33c3d37fd4f52d3
Kazuma Nara <nkz9528w.econ@gmail.com>
2025-03-15 13:01:47 +0900

    w

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,33 @@ import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
+import react from "@vitejs/plugin-react";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  base: "./",
+  // base: "./",
   build: {
     lib: {
       entry: resolve(__dirname, "lib/index.tsx"),
       name: "WorkerLib",
       // 適切な拡張子が追加されます
       fileName: "index",
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "react/jsx-runtime"],
+      // output: {
+      //   globals: {
+      //     react: "react",
+      //     reactDom: "react-dom",
+      //     "react/jsx-runtime": "react/jsx-runtime",
+      //   },
+      // },
     },
   },
   worker: {
     format: "es",
   },
-  plugins: [dts({ rollupTypes: true })],
+  plugins: [react(), dts({ rollupTypes: true })],
 });

d94ae31c4758a406a2e895213c23520717bdce0e
Kazuma Nara <nkz9528w.econ@gmail.com>
2025-03-15 16:11:04 +0900

    aaa

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,14 @@ import react from "@vitejs/plugin-react";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: resolve(__dirname, "lib/index.tsx"),
+      output: {
+        entryFileNames: "assets/[name].js",
+      },
+    },
+  },
   // build: {
   //   lib: {
   //     entry: resolve(__dirname, "lib/index.tsx"),
